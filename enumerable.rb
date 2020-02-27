04f5ec42abd0c040be2c5ad88ab030f7904359f0
Luis Saavedra <nriqu322@gmail.com>
2020-02-27 11:59:40 -0600

    added my_all? method and tested

--- enumerable.rb
+++ enumerable.rb
@@ -26,6 +26,14 @@ module Enumerable
     self.my_each { |x| new_array.push(x) if yield(x) }
     new_array
   end
+
+  def my_all?
+    #evaluates all elements in array and return true if all of them meets the given condition
+    return to_enum unless block_given?
+    result = true
+    self.my_each { |x| result = false if !yield(x)}
+    result
+  end
 end
 
 array = %w[5 2 7 4 1]
@@ -43,7 +51,15 @@ array = %w[5 2 7 4 1]
 #puts
 
 #test my_select
-array.select { |x| puts x if x.to_i < 5 }
+#array.select { |x| puts x if x.to_i < 5 }
+#puts
+#array.my_select { |x| puts x if x.to_i < 5 }
+#puts
+
+array_two = %w[house car bottle cloud candle]
+
+#test my_all?
+array_two.all? { |x| x.length <= 6 }
 puts
-array.my_select { |x| puts x if x.to_i < 5 }
-puts
+array_two.my_all? { |x| x.length <= 6 }
+#puts
