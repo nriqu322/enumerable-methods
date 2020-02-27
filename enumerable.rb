f91111db0c681022895524a388f7c70af50709da
Luis Saavedra <nriqu322@gmail.com>
2020-02-26 21:03:45 -0600

    method my_each_with_index added and tested

--- enumerable.rb
+++ enumerable.rb
@@ -8,19 +8,24 @@ module Enumerable
     end
   end
 
-  # test my_each
-  array = %w[1 2 3 4 5]
-  array.each { |_x| puts. x.to_i * 2 }
-  puts
-  array.my_each { |x| puts x.to_i * 2 }
-  puts
-
   def my_each_with_index
     # iterate trough an object
-    i = 0
-    while i < length
-      yield(self[idx])
-      i += idx
+    idx = 0
+    while idx < length
+      yield(self[idx], idx)
+      idx += 1
     end
   end
 end
+
+# test my_each
+# array = %w[5 2 7 4 1]
+# array.each { |x| puts x.to_i * 2 }
+# puts
+# array.my_each { |x| puts x.to_i * 2 }
+
+# test my_each_with_index
+# array.each_with_index { |val, idx| puts "value #{val} : idx #{idx}" }
+# puts
+# array.my_each_with_index { |val, idx| puts "value #{val} : idx #{idx}" }
+# puts
